3dc7b30fb61b14b76f3a03cc4cbb5076f5dd044b
olublessed <olu@olublessed.com>
2018-08-03 19:33:15 -0500

    updated variable.tf

--- pipeline/terraform/variables.tf
+++ pipeline/terraform/variables.tf
@@ -8,9 +8,9 @@ variable "create_bastion" {
   default = false
 }
 
-#variable "certificate_arn" {
-#  description = "HTTPS certificate ARN from the AWS Certificate Manager service"
-#}
+variable "certificate_arn" {
+  description = "HTTPS certificate ARN from the AWS Certificate Manager service"
+}
 
 variable "ecs_instance_type" {
   description = "ec2 instance type used by ecs container hosts"
