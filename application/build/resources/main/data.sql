e1086d6fd40593a38c1d96a279a67d519df4b422
Justin Wilcoxen <justin.wilcoxen@icf.com>
2018-08-07 08:35:14 -0400

    Restructured folders and added READMEs

--- application/build/resources/main/data.sql
+++
@@ -1,4 +0,0 @@
--- Default data loaded into H2 in-memory database started by Spring boot
-INSERT INTO movie (id, title, published_date, rating) VALUES (99, 'Indiana Jones', current_date(), 'A+')
-INSERT INTO movie (id, title, published_date, rating) VALUES (98, 'Back to Future', current_date(), 'A+')
-INSERT INTO movie (id, title, published_date, rating) VALUES (97, 'Star Wars', current_date(), 'C+')
