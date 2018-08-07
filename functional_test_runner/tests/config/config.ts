c2fe2a9431a82c69df36a3d939066dfa0b5d9b6a
charles-pinkston <charles.pinkston@icf.com>
2018-08-07 16:07:40 -0400

    [CEP][E2E] - Re-adding files for the E2E testing in a new repo and branch.

---
+++ functional_test_runner/tests/config/config.ts
@@ -0,0 +1,34 @@
+import * as path from "path";
+import { browser, Config } from "protractor";
+import { Reporter } from "../support/reporter";
+const functionalTestReports = path.join(process.cwd(), "../../","testing_quality_reports/functional");
+export const config: Config = {
+    seleniumAddress: "http://127.0.0.1:4444/wd/hub",
+    SELENIUM_PROMISE_MANAGER: false,
+    baseUrl: "https://stage.eagle.e3si.icfcloud.com/",
+    capabilities: {
+        browserName: "chrome",
+    },
+    framework: "custom",
+    frameworkPath: require.resolve("protractor-cucumber-framework"),
+    specs: [
+        "../../features/**/*.feature",
+    ],
+    onPrepare: () => {
+        // browser.ignoreSynchronization = true;
+        // browser.manage().window().maximize();
+        console.log("[CEP] - Running tests from " + process.cwd());
+        console.log("      - Attempting to create test results folder in " + functionalTestReports);
+        Reporter.createDirectory(functionalTestReports);
+    },
+    cucumberOpts: {
+        compiler: "ts:ts-node/register",
+        format: "json:"+ functionalTestReports + "/cucumber_report.json",
+        require: ["../../typeScript/step_definitions/*.js", "../../typeScript/support/*.js"],
+        strict: true,
+        //tags: "@validation",
+    },
+    onComplete: () => {
+        Reporter.createHTMLReport();
+    },
+};
---
+++ functional_test_runner/tests/support/reporter.ts
@@ -0,0 +1,35 @@
+import * as reporter from "cucumber-html-reporter";
+import * as fs from "fs";
+import * as mkdirp from "mkdirp";
+import * as path from "path";
+const functionalTestReports = path.join(process.cwd(), "../../","testing_quality_reports/functional");
+const targetJson = functionalTestReports + "/cucumber_report.json";
+const cucumberReporterOptions = {
+    jsonFile: targetJson,
+    theme: "hierarchy",
+    output: functionalTestReports + "/index.html",
+    reportSuiteAsScenarios: true,
+    screenshotsDirectory: 'screenshots',
+    storeScreenshots: true,
+    brandTitle: "ICF Proposal E2E Tests",
+    metadata: {
+        "Project":  "USCIS - xxxxxxxxxxx"
+    }
+};
+export class Reporter {
+    public static createDirectory(dir: string) {
+        if (!fs.existsSync(dir)) {
+            mkdirp.sync(dir);
+        }
+    }
+
+    public static createHTMLReport() {
+        try {
+            reporter.generate(cucumberReporterOptions); // invoke cucumber-html-reporter
+        } catch (err) {
+            if (err) {
+                throw new Error("Failed to save cucumber test results to json file.");
+            }
+        }
+    }
+}
